92a374ceaf6dcefe0e94f9130bb8ba6eefeb8a97
Joey Nguyen <jnn193@gmail.com>
2015-10-19 22:12:48 -0500

    Integrated MongoDB. Requested data from Mongo and displayed it on website.

--- app.js
+++ app.js
@@ -2,41 +2,29 @@ var express = require('express'),
     app = express(),
     cons = require('consolidate'),
     MongoClient = require('mongodb').MongoClient,
+    MongoServer = require('mongodb').Server,
     PORT = 8080;
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+var mongoclient = new MongoClient(new MongoServer('localhost', 27017, {'native_parser': true}));
+var db = mongoclient.db('course');
+
 app.get('/', function(req,res) {
-    res.render('hello', {'name': 'Swig'});
+    db.collection('hello_mongo_express').findOne({}, function(err, doc) {
+        res.render('hello', doc);
+    });
 });
 
 // Handle all routes not handled previously ^^
 app.get('*', function(req,res) {
     res.send("Result not found", 404);
 });
+mongoclient.open(function(err, mongoclient) {
+    if (err) throw err;
 
-app.listen(PORT);
-console.log('Server listening at http://localhost:' + PORT);
-
-// Open the connection to the server
-/*
-MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
-    if(err) throw err;
-
-    // Find one document in our collection
-    db.collection('coll').findOne({}, function(err, doc) {
-        if(err) throw err;
-
-        // Print the result
-        console.dir(doc);
-
-        // Close the db
-        db.close();
-    });
-
-    // Declare success
-    console.dir("Called findOne!");
+    app.listen(PORT);
+    console.log('Server listening at http://localhost:' + PORT);
 });
-*/
