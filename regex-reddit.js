553eb837fe9695e17bde820d1bc8eec95eca243a
Joey Nguyen <jnn193@gmail.com>
2015-10-26 21:15:25 -0500

    Created example of using  to filter results.

---
+++ regex-reddit.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
+    if (err) throw err;
+
+    var query = { title: {$regex: 'NSA'} };
+    var projection = {title: 1, '_id': 0};
+
+    db.collection('reddit').find(query, projection).each(function(err, doc) {
+        if (err) throw err;
+
+        if (doc !== null) {
+            console.dir(doc);
+        }
+
+        return db.close();
+    });
+});
