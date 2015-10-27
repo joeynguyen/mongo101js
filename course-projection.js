f584f3a0e00ac58a85f7a48af7fc83595756fdb0
Joey Nguyen <jnn193@gmail.com>
2015-10-26 19:17:54 -0500

    Implemented use of  and .

--- course-projection.js
+++ course-projection.js
@@ -3,15 +3,14 @@ var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
     if (err) throw err;
 
-    var query = { 'grade': 100 };
-    var projection = {'student': 1, '_id': 0};
+    var query = { 'grade': {$gt: 80, $lt: 95 } };
+    var projection = {'student': 1, 'grade': 1, '_id': 0};
 
     db.collection('grades').find(query, projection).toArray(function(err, docs) {
         if (err) throw err;
 
         docs.forEach(function(doc) {
             console.dir(doc);
-            console.dir(doc.student + " got a good grade!");
         });
 
         db.close();
